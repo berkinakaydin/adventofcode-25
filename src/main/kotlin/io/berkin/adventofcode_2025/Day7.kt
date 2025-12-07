233f5ad833038c0934f6f59b053275c1d234872f
Berkin Akaydin <berkin.akaydin@justeattakeaway.com>
2025-12-07 13:53:48 +0100

    day 7 init

---
+++ src/main/kotlin/io/berkin/adventofcode_2025/Day7.kt
@@ -0,0 +1,21 @@
+package io.berkin.adventofcode_2025
+
+import io.berkin.AbstractDay
+
+class Day7 : AbstractDay() {
+    override fun question1(): Any {
+        var output = 0L
+        val vectorRow = listOf(0, 1, 1, 1, 0, -1, -1, -1, -1)
+        val vectorColumn = listOf(1, 1, 0, -1, -1, -1, 0, 1)
+
+
+
+        return output
+    }
+
+    override fun question2(): Any {
+        var output = 0L
+
+        return output
+    }
+}
---
+++ src/test/kotlin/io/berkin/adventofcode_2025/Day7Test.kt
@@ -0,0 +1,38 @@
+package io.berkin.adventofcode_2025
+
+import io.berkin.AbstractDay
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.*
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class Day7Test {
+
+    private lateinit var day: AbstractDay
+    private var start: Long = 0L
+
+    @BeforeAll
+    fun init() {
+        day = Day7()
+    }
+
+    @Test
+    fun question1() {
+        Assertions.assertThat(day.question1()).isEqualTo(21)
+    }
+
+    @Test
+    fun question2() {
+        Assertions.assertThat(day.question2()).isEqualTo(11419862653216)
+    }
+
+    @BeforeEach
+    fun beforeEach() {
+        start = System.nanoTime()
+    }
+
+    @AfterEach
+    fun afterEach(testInfo: TestInfo) {
+        val end = System.nanoTime()
+        println("${testInfo.testClass.get().name} ${testInfo.testMethod.get().name} - Time taken: ${(end - start).toDouble() / 1_000_000}ms")
+    }
+}
